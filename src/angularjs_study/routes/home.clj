e1e462105a9ccbed853609fedc0c1f5aea1284ee
Ponkore <ponkore@gmail.com>
2014-01-21 22:17:05 +0900

    add project files(second commit)

---
+++ src/angularjs_study/routes/home.clj
@@ -0,0 +1,15 @@
+(ns angularjs-study.routes.home
+  (:use compojure.core)
+  (:require [angularjs-study.views.layout :as layout]
+            [angularjs-study.util :as util]))
+
+(defn home-page []
+  (layout/render
+    "home.html" {:content (util/md->html "/md/docs.md")}))
+
+(defn about-page []
+  (layout/render "about.html"))
+
+(defroutes home-routes
+  (GET "/" [] (home-page))
+  (GET "/about" [] (about-page)))
