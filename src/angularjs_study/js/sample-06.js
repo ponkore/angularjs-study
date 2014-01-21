0dd6a18245ffd08f3c4ca2af8241ba9def6d7b8b
Ponkore <ponkore@gmail.com>
2014-01-22 01:12:48 +0900

    sample angular js file moved into clojure's src tree.

--- src/angularjs_study/js/sample-06.js
+++ src/angularjs_study/js/sample-06.js
@@ -1,10 +1,12 @@
 angular.module('myapp', ['ui.bootstrap', 'ngGrid']);
 var GridDemoCtrl = function ($scope) {
-  $scope.myData = [{name: "吹雪", age: 50},
-                   {name: "赤城", age: 43},
-                   {name: "金剛", age: 27},
-                   {name: "島風", age: 29},
-                   {name: "雷", age: 34}];
+  $scope.myData = [
+      {name: "吹雪", age: 50},
+      {name: "赤城", age: 43},
+      {name: "金剛", age: 27},
+      {name: "島風", age: 29},
+      {name: "雷", age: 34}
+  ];
   $scope.myOptions = {
       data: 'myData',
       columnDefs: [
