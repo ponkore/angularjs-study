d006ec5352f676b564e9b40713f17f7fa72aed4d
ponkore <ponkore@gmail.com>
2014-06-17 23:02:51 +0900

    library version up.

--- project.clj
+++ project.clj
@@ -2,15 +2,15 @@
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :dependencies [[org.clojure/clojure "1.5.1"]
-                 [lib-noir "0.8.3"]
+                 [lib-noir "0.8.4"]
                  [compojure "1.1.8"]
                  [ring-server "0.3.1"]
-                 [selmer "0.6.6"]
+                 [selmer "0.6.7"]
                  [com.taoensso/timbre "3.2.1"]
                  [com.postspectacular/rotor "0.1.0"]
                  [com.taoensso/tower "2.0.2"]
                  [markdown-clj "0.9.44"]
-                 [environ "0.4.0"]
+                 [environ "0.5.0"]
                  [org.clojure/java.jdbc "0.3.3"]
                  [c3p0/c3p0 "0.9.1.2"]
                  [c3p0/c3p0-oracle-thin-extras "0.9.0.2"]
@@ -27,6 +27,6 @@
                        :stacktraces?  false
                        :auto-reload?  false}}
    :dev {:dependencies [[ring-mock "0.1.5"]
-                        [ring/ring-devel "1.2.2"]]
+                        [ring/ring-devel "1.3.0"]]
          :env {:selmer-dev true}}}
   :min-lein-version "2.0.0")
